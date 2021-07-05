7485c0133597af7889b8b27b8a784f84f45a8925
Allan <fl199594@gmail.com>
2021-07-05 15:37:21 -0400

    made every song show in the story menu
    i don't fucking know

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -459,7 +459,7 @@ class StoryMenuState extends MusicBeatState
 		{
 			txtTracklist.text += "\n" + i;
 		}
-
+		txtTracklist.text += "\n";
 		txtTracklist.text = txtTracklist.text.toUpperCase();
 
 		txtTracklist.screenCenter(X);
